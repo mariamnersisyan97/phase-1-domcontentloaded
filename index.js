87cfd30f55f5eb81bf8f66b818f8851215d4e4d8
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-05 18:31:10 -0500

    commit tree 40d9e4479ea4f6240c7a40c0a7599efa8046b1b0

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    let text = document.getElementById('text');
+    text.textContent = `This is really cool!`
+}
+)
